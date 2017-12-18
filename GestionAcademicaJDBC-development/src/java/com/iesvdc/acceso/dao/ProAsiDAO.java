adcf8ef53b72df722db7036bdc7b1472307c90bd
ElRorro <juravideos@gmail.com>
2017-12-18 10:14:43 +0100

    ProAsiDAO

--- GestionAcademicaJDBC-development/src/java/com/iesvdc/acceso/dao/ProAsiDAO.java
+++ GestionAcademicaJDBC-development/src/java/com/iesvdc/acceso/dao/ProAsiDAO.java
@@ -5,10 +5,15 @@
  */
 package com.iesvdc.acceso.dao;
 
+import com.iesvdc.acceso.pojo.Asignatura;
+import com.iesvdc.acceso.pojo.Profesor;
+import java.util.List;
+
 /**
  *
  * @author matinal
  */
 public interface ProAsiDAO {
-    
+    public List<Profesor> findProfesoresByAsignatura(Integer asignatura) throws DAOException;
+    public List<Asignatura> findAsignaturaByProfesor(Integer profesor) throws DAOException;
 }
